e2e549ac8fc45e7098c6e65c4c9ba514a9f469b1
Clarck <clarck0122@gmail.com>
2019-06-17 22:57:45 +0800

    test uploader

--- uploader.py
+++ uploader.py
@@ -9,6 +9,7 @@ class uploader():
     self.client_secret = os.environ.get('Client_Secret')
     self.access_token = os.environ.get('access_token')
     self.refresh_token = os.environ.get('refresh_token')
+    self.album_id = os.environ.get('Album_ID')
     self.client = ImgurClient(self.client_id, self.client_secret, self.access_token, self.refresh_token)
 
   def upload_photo(self, image_url, album):
@@ -30,6 +31,8 @@ class uploader():
 
 if __name__ == "__main__":
   print("refresh_token={}".format(os.environ.get('refresh_token')))
+  uploader = uploader()
+  uploader.upload_photo("https://i.imgur.com/wIkXb8f.jpg",uploader.album_id)
   # uploader = uploader()
   # uploader.upload_photo("http://images.performgroup.com/di/library/omnisport/d/49/rose-derrick-usnews-getty-ftr_g3codycqfuf91veyq2qtvdma1.jpg?t=-1980713401&w=960&quality=70",'P8Kuvtc')
 
