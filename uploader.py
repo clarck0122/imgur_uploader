1964997c1723b167be7737c88072023ed134db43
Clarck <clarck0122@gmail.com>
2019-07-31 00:27:02 +0800

    for depoly

--- uploader.py
+++ uploader.py
@@ -9,18 +9,18 @@ config.read("config.ini")
 class uploader():
 
   def __init__(self):
-    # self.client_id = os.environ.get('Client_ID')
-    # self.client_secret = os.environ.get('Client_Secret')
-    # self.access_token = os.environ.get('access_token')
-    # self.refresh_token = os.environ.get('refresh_token')
-    # self.album_id = os.environ.get('Album_ID')
-    # self.mashape_key = os.environ.get('Mashape_key')
-    self.client_id = config['imgur_api']['Client_ID']
-    self.client_secret = config['imgur_api']['Client_Secret']
-    self.access_token = config['imgur_api']['access_token']
-    self.refresh_token = config['imgur_api']['refresh_token']
-    self.album_id = config['imgur_api']['Album_ID']
-    self.mashape_key = config['imgur_api']['Mashape_key']
+    self.client_id = os.environ.get('Client_ID')
+    self.client_secret = os.environ.get('Client_Secret')
+    self.access_token = os.environ.get('access_token')
+    self.refresh_token = os.environ.get('refresh_token')
+    self.album_id = os.environ.get('Album_ID')
+    self.mashape_key = os.environ.get('Mashape_key')
+    # self.client_id = config['imgur_api']['Client_ID']
+    # self.client_secret = config['imgur_api']['Client_Secret']
+    # self.access_token = config['imgur_api']['access_token']
+    # self.refresh_token = config['imgur_api']['refresh_token']
+    # self.album_id = config['imgur_api']['Album_ID']
+    # self.mashape_key = config['imgur_api']['Mashape_key']
     # self.client = ImgurClient(self.client_id, self.client_secret, self.access_token, self.refresh_token)
     self.client = ImgurClient(self.client_id, self.client_secret, self.access_token, self.refresh_token, self.mashape_key)
 
